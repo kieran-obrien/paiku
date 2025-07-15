594095dea038b8abf54d7da954387dbbf8c2c7e1
Kieran O'Brien <hello@kieranobrien.dev>
2025-07-15 01:03:07 +0100

    Complete simplification of rendering,re-name files.

---
+++ e_ink_display_dev.py
@@ -0,0 +1,30 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+from haiku_gen import generate_haiku
+
+
+# Create separate images for black and red
+image_black = Image.new('1', (250, 122), 255)
+image_red = Image.new('1', (250, 122), 255)
+
+draw_black = ImageDraw.Draw(image_black)
+draw_red = ImageDraw.Draw(image_red)
+
+# Load font
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+font_path = os.path.join(BASE_DIR, 'fonts', 'Roboto.ttf')
+font_size = 16
+font = ImageFont.truetype(font_path, font_size)
+
+# Generate haiku
+haiku = generate_haiku().splitlines()
+
+draw_black.text((8, 18), f"{haiku[0]}", font = font, fill = 0)
+draw_red.text((8, 51), f"{haiku[1]}", font = font, fill = 0)
+draw_black.text((8, 84), f"{haiku[2]}", font = font, fill = 0)
+draw_red.rectangle((2, 2, 247, 119), outline=0)
+
+image_black.save("haiku_black.png")
+image_red.save("haiku_red.png")
+
+# TEST FILE, SAVES PILLOW IMGS LOCALLY
