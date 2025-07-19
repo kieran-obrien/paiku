929a6eff834f711960cb1a8cfd91da5bf8a49e0d
Kieran O'Brien <hello@kieranobrien.dev>
2025-07-19 23:00:30 +0100

    Project rename, remove .venv from repo

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kieran/Projects/pitest/.venv
+set -gx VIRTUAL_ENV /home/kieran/Projects/pyku/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
